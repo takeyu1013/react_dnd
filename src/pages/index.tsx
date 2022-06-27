10f4d7225093c34c4c4fd7c760d0ec3f70770dcb
takeyu1013 <yuto1013@gmail.com>
2022-06-27 23:40:43 +0900

    ♻️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,41 @@
 import type { NextPage } from "next";
-import type { FC } from "react";
-import type { Identifier } from "dnd-core";
 
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, FC } from "react";
 import { useDrag, useDrop, DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 const ITEM = "item" as const;
-const Item: FC<{
+
+type Item = {
   id: number;
   text: string;
-  index: number;
-  move: (dragIndex: number, index: number) => void;
-}> = ({ id, text, index, move }) => {
+};
+
+const Item: FC<
+  Item & {
+    index: number;
+    move: (dragIndex: number, index: number) => void;
+  }
+> = ({ id, text, index, move }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [{ handlerId }, drop] = useDrop<
-    {
-      index: number;
-    },
-    void,
-    { handlerId: Identifier | null }
-  >({
+  const [, drag] = useDrag<Pick<Item, "id"> & { index: number }>({
+    type: ITEM,
+    item: { id, index },
+  });
+  const [, drop] = useDrop<{ index: number }>({
     accept: ITEM,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      };
-    },
     hover(item, monitor) {
       if (!ref.current) {
         return;
       }
-
       const { index: dragIndex } = item;
       if (dragIndex === index) {
         return;
       }
-
       const clientOffset = monitor.getClientOffset();
       if (!clientOffset) {
         return;
       }
-
       const { top, bottom } = ref.current.getBoundingClientRect();
       const hoverMiddleY = (bottom - top) / 2;
       const hoverClientY = clientOffset.y - top;
@@ -51,40 +45,20 @@ const Item: FC<{
       if (dragIndex > index && hoverClientY > hoverMiddleY) {
         return;
       }
-
       move(dragIndex, index);
       item.index = index;
     },
   });
-  const [{ isDragging }, drag] = useDrag({
-    type: ITEM,
-    item: () => {
-      return { id, index };
-    },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
-  const opacity = isDragging ? 0 : 1;
 
   useEffect(() => {
     drag(drop(ref));
   });
 
-  return (
-    <div ref={ref} style={{ opacity }} data-handler-id={handlerId}>
-      {text}
-    </div>
-  );
+  return <div ref={ref}>{text}</div>;
 };
 
 const Home: NextPage = () => {
-  const [items, setItems] = useState<
-    {
-      id: number;
-      text: string;
-    }[]
-  >([
+  const [items, setItems] = useState<Item[]>([
     {
       id: 1,
       text: "Write a cool JS library",
@@ -107,27 +81,31 @@ const Home: NextPage = () => {
     },
   ]);
 
-  const move = (dragIndex: number, index: number) => {
-    type Item = {
-      id: number;
-      text: string;
-    };
-    const splice = (
-      input: Item[],
-      start: number,
-      deleteCount: number,
-      ...items: Item[]
-    ) =>
-      input.slice(0, start).concat(...items, input.slice(start + deleteCount));
-    setItems((items) =>
-      splice(splice(items, dragIndex, 1), index, 0, items[dragIndex])
-    );
-  };
-
   return (
     <DndProvider backend={HTML5Backend}>
       {items.map(({ id, text }, index) => {
-        return <Item key={id} id={id} text={text} index={index} move={move} />;
+        return (
+          <Item
+            key={id}
+            id={id}
+            text={text}
+            index={index}
+            move={(dragIndex, index) => {
+              const splice = (
+                input: Item[],
+                start: number,
+                deleteCount: number,
+                ...items: Item[]
+              ) =>
+                input
+                  .slice(0, start)
+                  .concat(...items, input.slice(start + deleteCount));
+              setItems((items) =>
+                splice(splice(items, dragIndex, 1), index, 0, items[dragIndex])
+              );
+            }}
+          />
+        );
       })}
     </DndProvider>
   );
